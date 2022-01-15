53cd6ba707c10f21f458b2a4b8f74d0e6cf1a067
Miranda <mirjade2008@yahoo.com>
2022-01-15 15:03:25 -0500

    update

--- app.js
+++ app.js
@@ -43,20 +43,47 @@
 
 var shop = [
     {
-      title: 'Dog',
+      title: 'Dogs',
+      image: './puppy for sale.jpg',
+      price: '199.99',
+      description: 'Our puppies are rescues from the local SPCA '
+    },
+
+    {
+      title: 'Dog food',
       image: './purina dog food.jpg',
-      price: '39.99',
+      price: '39.99 & under',
       description: 'Premium brands for all walks of life'
     },
+
+
+
     {
-      title: 'Cat',
+      title: 'Cats',
+      image: './kitten for sale.jfif',
+      price: "89.99",
+      description: "Our kittens are rescues from the local SPCA"
+    },
+    
+    {
+      title: 'Cat food',
       image: './cat food for sale.jpg',
       price: "29.99",
       description: "Quality food for all stages"
     },
-    
+
+
     {
-      title: 'Bird',
+      title: 'Birds',
+      image: './parakeet for sale.jpg',
+      price: "39.99",
+      description: "We have a beautiful selection of parakeets & more!"
+    },
+
+
+
+    {
+      title: 'Bird feed',
       image: './bird food for sale.jpg',
       price: "8.99",
       description: "Varieties for all species"
@@ -65,22 +92,22 @@ var shop = [
     {
       title: 'Small Pet',
       image: './small pet food for sale.jpg',
-      price: "5.99=11.99",
+      price: "5.99-$11.99",
       description: "Select from an array of varieties"
     },
 
     {
       title: 'Dog accessories',
       image: './dog accessories.jpg',
-      price: "2.99-19.99",
+      price: "2.99-$19.99",
       description: "Collars, leashes, toys and more!"
     },
     
     {
       title: 'Cat accessories',
       image: './cat accessories.jpg',
-      price: "1.99-10.99",
-      description: ""
+      price: "1.99-$10.99",
+      description: "A cats paradise!"
     },
 
     {
@@ -88,6 +115,13 @@ var shop = [
       image: './small pet supplies.jpg',
       price: "8.99",
       description: "Everything you need to care for your little one!"
+    },
+    
+    {
+      title: 'Deal of the Week',
+      image: './dog shampoo.png',
+      price: "BOGO @ $12.99",
+      description: "Organic dog shampoo"
     },
     
     
@@ -116,7 +150,7 @@ var shop = [
         var heading = '<div class="product"><span><h5>' + shop[i].title + '</h5>'
         var image = '<img src="' + shop[i].image + '" />'
         var price = '<p class="price"> $' + shop[i].price + '</p></span>'
-        var description = '<div class="description"><p>'+ shop[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
+        var description = '<div class="description"><p>'+ shop[i].description + '</p><button type="button" class="btn btn-warning">Shop Around!</button></div></div>'
         var concatThis = heading + image + price + description;
         postHTML = postHTML + concatThis
     }
